0d7072fd834ee3ab205e60a97bf89f2fb0dc0502
Ian MacIntosh <ianjmacintosh@gmail.com>
2017-04-08 13:14:58 -0400

    Call it `chess` dang it, not `game`

--- js/chessmaestro.js
+++ js/chessmaestro.js
@@ -1,5 +1,5 @@
 (function() {
-	var game = new Chess(),
+	var chess = new Chess(),
 		boardOptions = {
 			"draggable": true,
 			"position": "start",
@@ -9,14 +9,14 @@
 
 	function moveRandomPiece() {
 		// Make a list of all the moves
-		var moves = game.moves();
+		var moves = chess.moves();
 
 		// Get the current board position
 		// A few options here for what could be a good board position:
 		// ## chessboard.js
 		// ## chess.js
 		var move = moves[Math.floor(Math.random() * moves.length)];
-		game.move(move);
+		chess.move(move);
 
 		// Assign a score to the current board position, reset the board "high score"
 		// Try out the first move
@@ -24,15 +24,15 @@
 		// Cycle through list of moves
 		// Once all moves have been cycled through, perform the best move
 
-		board.position(game.fen());
+		board.position(chess.fen());
 
-		if (game.in_checkmate() === true) {
+		if (chess.in_checkmate() === true) {
 			alert("Checkmate! You lose!");
 		}
 	}
 
 	function reactToMove(source, target) {
-		var move = game.move({
+		var move = chess.move({
 			"from": source,
 			"to": target,
 			"promotion": "q"
@@ -41,7 +41,7 @@
 		if (move === null) {
 			return "snapback";
 		}
-		if (game.in_checkmate() === true) {
+		if (chess.in_checkmate() === true) {
 			alert("Checkmate! You win!");
 			return;
 		}
